e63c9e676315b1a7dfaa06d9408d11d1c7e46ca2
jeffjb4488 <66730109+jeffjb4488@users.noreply.github.com>
2021-12-03 23:51:34 -0500

    Update SliderControl.js

--- js/SliderControl.js
+++ js/SliderControl.js
@@ -337,8 +337,6 @@ L.control.sliderControl = function (options) {
     return new L.Control.SliderControl(options);
 };
 
-map.on('popupopen', function(e) {
-    var px = map.project(e.target._popup._latlng); // find the pixel location on the map where the popup anchor is
-    px.y -= e.target._popup._container.clientHeight/2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
-    map.panTo(map.unproject(px),{animate: true}); // pan to new center
+map.on('popupopen', function (marker) {
+    map.setView(marker.target._popup._latlng, e.target._zoom);
 });
